4955677fbe4382e454b32e7c7b9c7df492ac3047
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-12-04 20:21:01 -0600

    Added circadian and and melpa package archive. Reordered some stuff as well

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,29 +1,3 @@
-;;; I prefer cmd key for meta
-(if(eq system-type 'darwin)
-    (setq mac-option-key-is-meta nil
-	  mac-command-key-is-meta t
-	  mac-command-modifier 'meta
-	  mac-option-modifier 'none)
-  )
-
-;; Startup stuff
-(defun my-startup-hook ()
-  (server-start)
-  (load-theme 'leuven t)
-  (when (version<= "26.0.50" emacs-version )
-    (global-display-line-numbers-mode))
-  )
-(add-hook 'emacs-startup-hook 'my-startup-hook)
-
-
-;; C customizations
-(defun my-c-mode-hook ()
-  (setq indent-tabs-mode nil)
-  (setq-default c-basic-offset 4)
-  (column-enforce-mode)
-  )
-(add-hook 'c-mode-hook 'my-c-mode-hook)
-
 ;; Melpa
 (require 'package)
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
@@ -41,7 +15,9 @@ There are two things you can do about this warning:
   ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))
+    (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+    ))
 (package-initialize)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -51,10 +27,48 @@ There are two things you can do about this warning:
  '(custom-safe-themes
    (quote
     ("40670c1a2158e4387407ca1c8a06c03153baca5016e3f9f23d7fb4157e408960" default)))
- '(package-selected-packages (quote (magit leuven-theme column-enforce-mode))))
+ '(package-selected-packages
+   (quote
+    (zenburn-theme circadian magit leuven-theme column-enforce-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;; I prefer cmd key for meta
+(if(eq system-type 'darwin)
+    (setq mac-option-key-is-meta nil
+	  mac-command-key-is-meta t
+	  mac-command-modifier 'meta
+	  mac-option-modifier 'none)
+  )
+
+;; Startup stuff
+(defun my-startup-hook ()
+  (server-start)
+  (scroll-bar-mode -1)
+  (when (version<= "26.0.50" emacs-version )
+    (global-display-line-numbers-mode))
+  )
+(add-hook 'emacs-startup-hook 'my-startup-hook)
+
+;; Circadian
+(use-package circadian
+  :ensure t
+  :config
+  (setq calendar-latitude 29.6)
+  (setq calendar-longitude -98.6)
+  (setq circadian-themes '((:sunrise . leuven)
+                            (:sunset  . zenburn)))
+  (circadian-setup))
+
+;; C customizations
+(defun my-c-mode-hook ()
+  (setq indent-tabs-mode nil)
+  (setq-default c-basic-offset 4)
+  (column-enforce-mode)
+  )
+(add-hook 'c-mode-hook 'my-c-mode-hook)
+
